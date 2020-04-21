b5011f0987805024df11d01d48499994ba2370c5
Archie <archie.isdiningrat@gmail.com>
2020-04-21 12:13:00 +0700

    improve emblem handling and add emblem notification

--- src/services/emblem_service.ts
+++ src/services/emblem_service.ts
@@ -1,17 +1,21 @@
 import UserEmblemRepository from '../repositories/user_emblem_repo';
 import Worker from '../jobs';
 import { EmblemAttachedData } from 'src/typings/worker';
+import { EMBLEM_CODE } from 'src/utils/constant';
 
 export default class EmblemService {
-    public static async attach(userId: string, emblemCode: string, active: boolean = false): Promise<void> {
+    public static async attach(userId: string, emblemCode: EMBLEM_CODE, active: boolean = false): Promise<void> {
         const userEmblemRepo = new UserEmblemRepository();
         const payload = { user_id: userId, emblem_code: emblemCode };
-
-        await userEmblemRepo.upsert(payload, { ...payload, is_active: active });
-
-        await Worker.dispatch<EmblemAttachedData>(Worker.Job.EMBLEM_ATTACHED, {
-            user_id: userId,
-            emblem_code: emblemCode
-        });
+        const emblem = await userEmblemRepo.findOne(payload);
+        if (!emblem) {
+            await Promise.all([
+                userEmblemRepo.create({ is_active: active, ...payload }),
+                Worker.dispatch<EmblemAttachedData>(Worker.Job.EMBLEM_ATTACHED, {
+                    user_id: userId,
+                    emblem_code: emblemCode
+                })
+            ]);
+        }
     }
 }
