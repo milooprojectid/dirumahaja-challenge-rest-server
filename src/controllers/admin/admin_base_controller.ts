6c26cdb205a685e9453b5eaf372434812b64851f
Archie <archie.isdiningrat@gmail.com>
2020-04-22 10:58:12 +0700

    add dashboard controller

--- src/controllers/admin/admin_base_controller.ts
+++ src/controllers/admin/admin_base_controller.ts
@@ -1,8 +1,10 @@
 import BaseController from '../base/base_controller';
 import AdminAuthController from './auth_controller';
+import AdminDashboardController from './dashboard_controller';
 
 export default class AdminBaseController extends BaseController {
     public setRoutes(): void {
         this.addChildController('/', new AdminAuthController());
+        this.addChildController('/dashboard', new AdminDashboardController());
     }
 }
