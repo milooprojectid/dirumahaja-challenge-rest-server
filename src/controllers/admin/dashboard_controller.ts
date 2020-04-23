6a40e89d6675631bcec3395e1f7b5a1a630cf522
Archie <archie.isdiningrat@gmail.com>
2020-04-23 15:33:40 +0700

    add map endpoint

--- src/controllers/admin/dashboard_controller.ts
+++ src/controllers/admin/dashboard_controller.ts
@@ -12,27 +12,49 @@ export default class AdminDashboardController extends BaseController {
     }
 
     public async statistic(data: IData, context: IContext): Promise<IHandlerOutput> {
-        const userRepo = new UserRepository(context);
+        try {
+            const userRepo = new UserRepository(context);
+            const [totalUser, ageCounter, genderCounter, locationCounter] = await Promise.all([
+                userRepo.count({}),
+                userRepo.countAttribute('age'),
+                userRepo.countAttribute('gender'),
+                userRepo.countAttribute('location_name')
+            ]);
 
-        const [totalUser, ageCounter, genderCounter, locationCounter] = await Promise.all([
-            userRepo.count({}),
-            userRepo.countAttribute('age'),
-            userRepo.countAttribute('gender'),
-            userRepo.countAttribute('location_name')
-        ]);
+            return {
+                message: 'dashboard data retrieved',
+                data: {
+                    total_user: totalUser,
+                    age: ageCounter,
+                    gender: genderCounter,
+                    location: locationCounter
+                }
+            };
+        } catch (err) {
+            if (err.status) throw err;
+            throw HttpError.InternalServerError(err.message);
+        }
+    }
 
-        return {
-            message: 'dashboard data retrieved',
-            data: {
-                total_user: totalUser,
-                age: ageCounter,
-                gender: genderCounter,
-                location: locationCounter
-            }
-        };
+    public async map(data: IData, context: IContext): Promise<IHandlerOutput> {
+        try {
+            const userRepo = new UserRepository(context);
+
+            const users = await userRepo.findAll({}, '-created_at', ['coordinate']);
+            const points = users.map((user): number[] => [...user.coordinate.coordinates]);
+
+            return {
+                message: 'coordinates retrieved',
+                data: points
+            };
+        } catch (err) {
+            if (err.status) throw err;
+            throw HttpError.InternalServerError(err.message);
+        }
     }
 
     public setRoutes(): void {
         this.addRoute('get', '/', this.statistic);
+        this.addRoute('get', '/map', this.map);
     }
 }
