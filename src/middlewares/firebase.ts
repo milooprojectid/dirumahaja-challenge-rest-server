2d34f9b59e1f812df649a2bf39dbfeb0651b21d6
Archie <archie.isdiningrat@gmail.com>
2020-04-28 00:19:43 +0700

    implement firebase auth middleware

--- src/middlewares/firebase.ts
+++ src/middlewares/firebase.ts
@@ -1,20 +1,31 @@
 /// <reference types="../typings/express" />
 
 import { Request, Response, NextFunction } from 'express';
-import { HttpError } from 'tymon';
+import { HttpError, FirebaseContext } from 'tymon';
 import { COMMON_ERRORS } from '../utils/constant';
-import { IContext } from 'src/typings/common';
+import { IContext, IFirebaseToken } from 'src/typings/common';
 
-const generateContext = (uid: string): IContext => ({
-    user_id: uid,
-    username: ''
+const generateContext = (user: IFirebaseToken): IContext => ({
+    user_id: user.uid,
+    username: user.uid
 });
 
-export default (req: Request, res: Response, next: NextFunction): void => {
-    const uid = req.query.uid || req.headers.uid;
-    if (!uid) {
+export default async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const idToken = req.headers.Authorization;
+    if (!idToken) {
         return next(HttpError.NotAuthorized(COMMON_ERRORS.TOKEN_INVALID));
     }
-    req.context = generateContext(uid);
+
+    const firebase = await FirebaseContext.getInstance();
+
+    /** verify firebase id token */
+    let user: IFirebaseToken;
+    try {
+        user = await firebase.auth().verifyIdToken(idToken);
+    } catch (err) {
+        throw HttpError.NotAuthorized('TOKEN_INVALID');
+    }
+
+    req.context = generateContext(user);
     return next();
 };
